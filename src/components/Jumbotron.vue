a85c78969bff51a9a34dc0433edcbb87fc72dbd5
Stepan <stepan.petrov@planisense.com>
2022-08-08 09:29:42 +0300

    new

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="container my-5">
     <div class="row">
-      <input type="checkbox" id='theme-switch' class='theme-switch button' v-model="darkMode"/>
-      <div class="center-block"><img src="../assets/images/main.png"  width="600" height="600" alt=""></div>
-      <div ><img src="../assets/images/mooner.png"  width="500" height="500" alt=""></div>
-      <h1 class="bg-gradient">Availability.</h1>
+
+      <div class="center-block">
+        <Logo/>
+        <img src="../assets/images/main.png"  width="600" height="600" alt=""></div>
       </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="left-block">
+          <img src="../assets/images/mooner.png"  width="500" height="500" alt="">
+        </div>
+      </div>
+      <div class="col-md-5">
+        <h1>
+          Hello
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="row">
+          <h1>
+            Hello
+          </h1>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="row">
+          <div class="right-block"><img src="../assets/images/pc-lover.png"  width="500" height="500" alt=""></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import Logo from './Logo.vue'
 export default {
+
   /* eslint-disable */
   name: 'Jumbotron',
+  components: {
+    Logo
+  },
   data: () => ({
     darkMode: false
   }),
@@ -40,6 +76,14 @@ export default {
 <style scoped>
 .center-block {
   text-align: center;
+  margin-top: 100px;
+}
+.left-block {
+  text-align: left;
+  margin-top: 100px;
+}
+.right-block {
+  text-align: right;
   margin-top: 100px;
 }
 </style>
